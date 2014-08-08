74e55632ad361ec6f3c7b205bd9a284afaada11f
Randy <randy.solomonson@sizmek.com>
2014-08-08 00:09:46 -0500

    Clarified the Spring Batch Partition Example.

--- src/main/java/com/solomonson/example/spring/batch/partition/ExamplePartitionJob.java
+++ src/main/java/com/solomonson/example/spring/batch/partition/ExamplePartitionJob.java
@@ -1,15 +1,18 @@
 package com.solomonson.example.spring.batch.partition;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.item.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +23,8 @@ import java.util.Map;
 @Configuration
 @EnableBatchProcessing
 public class ExamplePartitionJob {
+    private final Logger logger = LoggerFactory.getLogger(ExamplePartitionJob.class);
+
     public static class User{
         private String name;
         public User(String name){
@@ -34,16 +39,20 @@ public class ExamplePartitionJob {
         }
     }
 
-    @Autowired
+    @Inject
     private JobBuilderFactory jobBuilders;
 
-    @Autowired
+    @Inject
     private StepBuilderFactory stepBuilders;
 
     @Bean
     public Job mainJob() {
         return jobBuilders.get("mainJob")
+
+                //Start the job by running the main step
                 .start(masterStep())
+
+                //Optional:  Use a listener just to show stats.
                 .listener(new JobExecutionListener() {
                     @Override
                     public void beforeJob(JobExecution jobExecution) {
@@ -51,8 +60,8 @@ public class ExamplePartitionJob {
 
                     @Override
                     public void afterJob(JobExecution jobExecution) {
-                        for (StepExecution se:jobExecution.getStepExecutions()){
-                            System.out.printf("%s=%d reads\n", se.getStepName(), se.getReadCount());
+                        for (StepExecution se : jobExecution.getStepExecutions()) {
+                            logger.info("{}={} reads", se.getStepName(), se.getReadCount());
                         }
                     }
                 })
@@ -61,14 +70,17 @@ public class ExamplePartitionJob {
 
     @Bean
     public Step masterStep(){
-        org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor taskExecutor = new org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor();
+
+        //Create the thread pool that will be used.
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         taskExecutor.setCorePoolSize(2);
         taskExecutor.setMaxPoolSize(2);
         taskExecutor.afterPropertiesSet();
 
         return stepBuilders.get("masterStep")
-                .partitioner(slaveStep())
-                .partitioner("slaveStep", new Partitioner() {
+
+                //Break the input data into a map of stepName->context.
+                .partitioner("extraStep", new Partitioner() {
                     @Override
                     public Map<String, ExecutionContext> partition(int gridSize) {
                         Map<String, ExecutionContext> contexts = new HashMap<>();
@@ -82,26 +94,36 @@ public class ExamplePartitionJob {
                         return contexts;
                     }
                 })
+
+                //Define the slave step to run on all the of the contexts created above.
+                .step(slaveStep())
+
+                //Define the threading of the steps.
                 .taskExecutor(taskExecutor)
+
                 .build();
     }
 
     @Bean
     public Step slaveStep() {
-
+        //A basic step
         return stepBuilders.get("slaveStep")
                 .<User, User>chunk(1)
                 .reader(new ItemStreamReader<User>() {
+                    //Use some thread locals here to keep it all thread-safe (not sure if this is required)
+                    ThreadLocal<String> context=new ThreadLocal<>();
                     ThreadLocal<Integer> i = new ThreadLocal<>();
 
                     @Override
                     public void open(ExecutionContext executionContext) throws ItemStreamException {
+                        //The read() method doesn't give a context, so all context lookup is done here.
+                        context.set(executionContext.getString("key"));
                         i.set(0);
                     }
 
                     @Override
                     public void update(ExecutionContext executionContext) throws ItemStreamException {
-
+                        //Typically used for storing state into the context.
                     }
 
                     @Override
@@ -112,12 +134,24 @@ public class ExamplePartitionJob {
                     public User read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
                         i.set(i.get() + 1);
                         if (i.get() <= 10) {
-                            User user = new User(Integer.toString(i.get()));
+                            User user = new User(context.get()+Integer.toString(i.get()));
                             return user;
                         }
                         return null;
                     }
                 })
+
+                //Define what to do with each object being processed.
+                .processor(new ItemProcessor<User, User>() {
+                    @Override
+                    public User process(User item) throws Exception {
+                        logger.info("{}",item);
+                        Thread.sleep(1000);
+                        return item;
+                    }
+                })
+
+                //Write data to something defined in the writer itself
                 .writer(new ItemWriter<User>() {
                     @Override
                     public void write(List items) throws Exception {
@@ -127,5 +161,4 @@ public class ExamplePartitionJob {
                         //.throttleLimit(2)
                 .build();
     }
-
 }
