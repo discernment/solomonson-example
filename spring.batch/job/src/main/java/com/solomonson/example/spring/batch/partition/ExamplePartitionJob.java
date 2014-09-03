4fa81cd7e58dcb81750ae860724fa26425f874c0
Randy <randy.solomonson@sizmek.com>
2014-09-03 16:33:30 -0500

    Moved some folders

--- spring.batch/job/src/main/java/com/solomonson/example/spring/batch/partition/ExamplePartitionJob.java
+++ spring.batch/job/src/main/java/com/solomonson/example/spring/batch/partition/ExamplePartitionJob.java
@@ -16,8 +16,6 @@ import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
